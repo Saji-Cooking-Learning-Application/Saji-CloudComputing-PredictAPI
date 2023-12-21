518fe7c40e4364037ce4c629f1fb862e14fb54e2
Yudia Artana <yudiaartana87@gmail.com>
2023-12-21 15:58:27 +0800

    add env

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ model_bahan = load_model('modelbahan.h5')
 # Fungsi untuk menghubungkan ke database MySQL
 def mysql_connect():
     return mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='',
-        database='bangkit3'
+        host=os.environ.get('HOST'),
+        user=os.environ.get('USER'),
+        password=os.environ.get('PASSWORD'),
+        database=os.environ.get('DATABASE')
     )
 
 @app.route('/')
